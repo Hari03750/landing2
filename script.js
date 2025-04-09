7ef26288659eebd9c073453200558a6dfc7e46df
Hari03750 <hariharan2004b@gmail.com>
2025-04-09 23:15:47 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Website Loaded Successfully");
+
+  // Example script if you want to add interactivity
+  const btn = document.querySelector(".btn");
+  btn.addEventListener("click", () => {
+    alert("Welcome to the enhanced version!");
+  });
+});
